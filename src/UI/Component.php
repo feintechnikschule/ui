c06632dcff04f25df79debc2ac56b2ff96559bb1
fm <fm@feintechnikschule.de>
2024-08-28 12:41:26 +0200

    fix: insert component state as parameter in component->render using global state as default only

--- src/UI/Component.php
+++ src/UI/Component.php
@@ -2,18 +2,18 @@
 
 namespace Leaf\UI;
 
-class Component
+abstract class Component
 {
     /**
-     * @var string $key The unique key of the component
+     * @var string $key The unique key of the component must be set and always the same
      */
-    public $key;
+    public string $key;
 
     /**
-     * @return string
+     * @abstract All components must implement this method using their view and state for rendering
+     * @param $state The state of the component for rendering the view. If omitted the global state is used
+     * @return string The html string of the component
      */
-    public function render()
-    {
-        // 
+    abstract public function render($state = []): string;
     }
 }
